0a5262476853cca8d79fcc62136277249985cc8c
jbao8899 <jbao8899@gmail.com>
2023-07-09 22:48:32 -0600

    Created project for tests

---
+++ General Learning/Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using General_Learning;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
